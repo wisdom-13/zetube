c92ae800e1aa4bb3a162eb2ea0ffd347eb78a949
jhlee <wisdom_13_@naver.com>
2022-11-30 00:08:44 +0900

    비디오 상세보기 키 가져오기

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 export default function Videos() {
   const { keyword } = useParams();
-  return <div>Videos : {keyword}</div>
+  return (
+    <>
+      <h3>검색어 : {keyword}</h3>
+
+      <Link to='/videos/watch/jfweoifjwoefj'>
+        Vidoes1
+      </Link>
+      <br></br>
+      <Link to='/videos/watch/fewfwfwef'>
+        Vidoes2
+      </Link>
+    </>
+  );
 }
