be80927b044ebe3126c0925f1a3f9eca6e272ab7
jhlee <wisdom_13_@naver.com>
2022-12-05 23:53:41 +0900

    form 스타일 추가

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -17,8 +17,16 @@ export default function SearchBar() {
       <Link to='/'>
         <h1 className='text-3xl'>zetube</h1>
       </Link>
+
+
       <form onSubmit={heandelSubmit}>
-        <input type='text' value={keyword} onChange={((e) => setKeyword(e.target.value))} placeholder='Search'></input>
+        <input type="text" value={keyword} onChange={((e) => setKeyword(e.target.value))} placeholder='Search'
+          class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+          focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500
+          disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+          invalid:border-pink-500 invalid:text-pink-600
+          focus:invalid:border-pink-500 focus:invalid:ring-pink-500
+        "/>
       </form>
     </div>
   );
