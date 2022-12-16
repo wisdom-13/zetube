36075108c6b5c8d46027289d76c2da9954063c94
jhlee <wisdom_13_@naver.com>
2022-12-16 23:51:34 +0900

    client 파일 생성 (진행중)

---
+++ src/api/youtubeClient.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+export default class Youtube {
+  constructor() {
+    this.httpClient = axios.create({
+      baseURL: 'https://www.googleapis.com/youtube/v3',
+      params: { key: process.env.REACT_APP_YOUTUBE_API_KEY }
+    })
+  }
+
+  async search(keyword) {
+    return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
+  }
+
+  async #searchByKeyword(keyword) {
+    return this.httpClient
+      .get('search', {
+        params: {
+          part: 'snippet',
+          maxResults: 25,
+          type: 'video',
+          q: keyword,
+        }
+      })
+      .then((res) => res.data.items)
+      .then((items) => items.map((item) => ({ ...item, id: item.id.videoId })))
+  }
+
+  async #mostPopular() {
+    return this.httpClient
+      .get('videos', {
+        params: {
+          part: 'snippet',
+          chart: 'mostPopular',
+          maxResults: 25,
+        }
+      })
+      .then((res) => res.data.items)
+  }
+}
+
+
