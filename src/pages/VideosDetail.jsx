f4922345d91113c2fa38a10438ef89da2f48deaa
jhlee <wisdom_13_@naver.com>
2022-12-18 23:18:41 +0900

    비디오 카드 세부 정보 보여주기

--- src/pages/VideosDetail.jsx
+++ src/pages/VideosDetail.jsx
@@ -5,6 +5,11 @@ export default function VideoDetail({ video }) {
   // const { videoId } = useParams();
   return (
     // <div>VideoDetail : {videoId}</div>
-    <div>{video.snippet.title}</div>
+    <div>
+      <img src={video.snippet.thumbnails.high.url}></img>
+      <h3>{video.snippet.title}</h3>
+      <h5>{video.snippet.channelTitle}</h5>
+      <h5>{video.snippet.publishedAt}</h5>
+    </div>
   );
 }
