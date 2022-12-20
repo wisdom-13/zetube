7909908650917a44e2f22abec3656128109fdc36
jhlee <wisdom_13_@naver.com>
2022-12-20 23:24:10 +0900

    비디오 카드 스타일링

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -28,9 +28,11 @@ export default function Videos() {
       {isLoading && <p>Loading...</p>}
       {error && <p>error</p>}
 
-      {videos && <ul>
-        {videos.map(video => <VideoDetail key={video.id} video={video} />)}
-      </ul>}
+      {videos && (
+        <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-2 gap-y-4'>
+          {videos.map(video => <VideoDetail key={video.id} video={video} />)}
+        </ul>
+      )}
     </>
   );
 }
--- src/pages/VideosDetail.jsx
+++ src/pages/VideosDetail.jsx
@@ -8,11 +8,11 @@ export default function VideoDetail({ video }) {
 
   return (
     <li>
-      <img src={thumbnails.medium.url}></img>
+      <img className='w-full' src={thumbnails.medium.url}></img>
       <p>
-        <h3>{title}</h3>
-        <h5>{channelTitle}</h5>
-        <h5>{formatAgo(publishedAt, 'ko')}</h5>
+        <h3 className='font-semibold my-2 line-clamp-2'>{title}</h3>
+        <h5 className='text-sm opacity-80'>{channelTitle}</h5>
+        <h5 className='text-sm opacity-80'>{formatAgo(publishedAt, 'ko')}</h5>
       </p>
     </li>
   );
