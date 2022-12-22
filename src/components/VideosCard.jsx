0783e8900555ee652a99d74b4a975604675308f9
jhlee <wisdom_13_@naver.com>
2022-12-22 23:29:49 +0900

    상세 페이지 정보 받아오기

--- src/components/VideosCard.jsx
+++ src/components/VideosCard.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { formatAgo } from '../util/date';
 
 
 export default function VideosCard({ video }) {
 
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+
+  const navigate = useNavigate();
   const link = `/videos/watch/${video.id}`
 
   return (
-    <Link to={link}>
-      <li>
-        <img className='w-full' src={thumbnails.medium.url}></img>
-        <p>
-          <h3 className='font-semibold my-2 line-clamp-2'>{title}</h3>
-          <h5 className='text-sm opacity-80'>{channelTitle} / </h5>
-          <h5 className='text-sm opacity-80'>{formatAgo(publishedAt, 'ko')}</h5>
-        </p>
-      </li>
-    </Link>
+    <li onClick={() => navigate(link, { state: { video } })}>
+      <img className='w-full' src={thumbnails.medium.url}></img>
+      <p>
+        <h3 className='font-semibold my-2 line-clamp-2'>{title}</h3>
+        <h5 className='text-sm opacity-80'>{channelTitle} / </h5>
+        <h5 className='text-sm opacity-80'>{formatAgo(publishedAt, 'ko')}</h5>
+      </p>
+    </li>
   );
 }
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 export default function VideoDetail() {
 
   const { videoId } = useParams();
+  const { state: { video } } = useLocation();
 
-  return <div>VideoDetail: {videoId}</div>
+  return (
+    <div>
+      VideoDetail: {videoId}
+      <iframe type="text/html" width="100%" height="400px"
+        title="youtube video player"
+        src="https://www.youtube.com/embed/"
+        frameBorder="0" allowFullScreen></iframe>
+    </div>
+  )
 }
