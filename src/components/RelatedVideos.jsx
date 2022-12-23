102b68f8c4df2efc8ded6d02f9f5166762c356d5
jhlee <wisdom_13_@naver.com>
2022-12-23 23:42:45 +0900

    상세 페이지 골격 잡기

---
+++ src/components/RelatedVideos.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function RelatedVideos({ id }) {
+
+  return (
+    <h2>{id}</h2>
+  );
+}
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
 
 export default function VideoDetail() {
 
-  const { videoId } = useParams();
   const { state: { video } } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
 
   return (
-    <div>
-      VideoDetail: {videoId}
-      <iframe type="text/html" width="100%" height="400px"
-        title="youtube video player"
-        src="https://www.youtube.com/embed/"
-        frameBorder="0" allowFullScreen></iframe>
-    </div>
+    <section>
+      <article>
+        <iframe type="text/html" width="100%" height="400px"
+          title="youtube video player"
+          src={`https://www.youtube.com/embed/${video.id}`}
+          frameBorder="0" allowFullScreen></iframe>
+        <div>
+          <h2>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle}></ChannelInfo>
+          <pre>{description}</pre>
+        </div>
+      </article>
+      <section>
+        <RelatedVideos id={video.id}></RelatedVideos>
+      </section>
+    </section>
+
   )
 }
