05d81fd756d4d06a8f3d88e988c338c8dc46a2c3
jhlee <wisdom_13_@naver.com>
2023-08-25 23:02:39 +0900

    스냅샷 테스트

--- src/components/tests/VideosCard.test.jsx
+++ src/components/tests/VideosCard.test.jsx
@@ -1,26 +1,42 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import VideosCard from '../VideosCard'
-import { MemoryRouter, Route, Routes, useLocation } from 'react-router-dom'
-import { formatAgo } from '../../util/date'
+import { Route, useLocation } from 'react-router-dom'
+import renderer from 'react-test-renderer'
 import { fakeVideo as video } from '../../tests/videos'
 import { withRouter } from '../../tests/utils'
 
 describe('VideoCard', () => {
-  const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
+  // const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
 
-  it('renders video item', () => {
-    render(
+  // it('renders video item', () => {
+  //   render(
+  //     withRouter(<Route path='/' element={<VideosCard video={video} />} />)
+  //   )
+
+  //   const image = screen.getByRole('img');
+  //   expect(image.src).toBe(thumbnails.medium.url);
+  //   expect(image.alt).toBe(title);
+  //   expect(screen.getByText(title)).toBeInTheDocument();
+  //   expect(screen.getByText(channelTitle)).toBeInTheDocument();
+  //   expect(screen.getByText(formatAgo(publishedAt, 'ko'))).toBeInTheDocument();
+  // })
+
+  it('renders grid type correctly', () => {
+    const component = renderer.create(
       withRouter(<Route path='/' element={<VideosCard video={video} />} />)
-    )
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 
-    const image = screen.getByRole('img');
-    expect(image.src).toBe(thumbnails.medium.url);
-    expect(image.alt).toBe(title);
-    expect(screen.getByText(title)).toBeInTheDocument();
-    expect(screen.getByText(channelTitle)).toBeInTheDocument();
-    expect(screen.getByText(formatAgo(publishedAt, 'ko'))).toBeInTheDocument();
-  })
+  it('renders list type correctly', () => {
+    const component = renderer.create(
+      withRouter(<Route path='/' element={<VideosCard video={video} type='list' />} />)
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+
 
   it('navigates to detailed video page with wideo state when clicked', () => {
     function LocationStateDisplay() {
