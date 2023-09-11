fc3f7ac53e454ed266b29dc622c5398705b3f20a
jhlee <wisdom_13_@naver.com>
2023-09-11 20:36:27 +0900

    mock 데이터로 테스트하기

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -3,10 +3,24 @@ import '@testing-library/cypress/add-commands';
 
 describe('Youtube App', () => {
   beforeEach(() => {
+    cy.intercept('GET', /(mostPopular)/g, {
+      fixture: 'popular.json'
+    })
+    cy.intercept('GET', /(search)/g, {
+      fixture: 'search.json'
+    })
+    cy.intercept('GET', /(mostPopular)/g, {
+      fixture: 'popular.json'
+    })
+    cy.viewport(1200, 800);
     cy.visit('/')
   });
 
   it('renders', () => {
-    cy.findByText('Youtube').should('exist');
+    cy.findByText('zetube').should('exist');
+  })
+
+  it('shows popluar video first', () => {
+    cy.findByText('Popular video').should('exist')
   })
 })
